1eca20037b9635c592607fb7143768cb809a3b3b
hamish <hamishgeorgedavie@gmail.com>
2023-05-16 13:38:32 +1000

    Changes

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,22 @@
 ## code your solution here. 
+#Defining a Class called Cat 
+
+class Cat
+    #defining macro method cat name 
+    attr_accessor :name
+
+    #method meow
+    def meow
+        puts "meow!"
+    end
+
+    
+end
+
+#instance of our class 
+pretty = Cat.new
+#Calling our cats name
+pretty.name = "Pretty"
+puts pretty.name
+#Calling our method meow
+pretty.meow
